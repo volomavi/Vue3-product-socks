529126b49aeede16fd442d88a79e384f088b6a00
Ivan B <volomavi@hotmail.com>
2020-12-28 21:16:39 -0800

    computed properties

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ const app = Vue.createApp({
         return {
             cart: 0,
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            inStock: false,
+            brand: 'Vue',
+            selectedVariant: 0,
             details: ['50% Cotton', '30% Wool', '20% Polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0},
             ],
-            sizes: ['Small', 'Medium', 'Large']
+            sizes: ['Small', 'Medium', 'Large'],
+            onSale: true
+
         }
     },
 
@@ -23,8 +25,26 @@ const app = Vue.createApp({
             this.cart --;
         },
 
-        updateImage(variantImage) {
-            this.image = variantImage;
+        updateVariant(index) {
+            this.selectedVariant = index;
         }
+    },
+
+    computed: {
+        title() { 
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock()  {
+            return this.variants[this.selectedVariant].quantity
+        },
+        saleMessage() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' is on sale'
+            }
+        },
+        
     }
 })
